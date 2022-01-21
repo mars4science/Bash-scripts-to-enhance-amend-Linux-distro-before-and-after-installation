8b9c3f8a994c692708e28f7d127b92956ec83f8c
Alex Martian <alex4science4mars@gmail.comm>
2022-01-21 09:32:26 +0000

    disable updates

--- firefox-replace.sh
+++ firefox-replace.sh
@@ -23,4 +23,9 @@ sudo tar --extract --file=$ff_archive
 # restore link that the srcipt code as written is supposed to break
 if [ $ff_link_symbolic = "true" ]; then sudo ln -s $ff_installed_folder/firefox $ff_installed_link; fi
 
+# disable updates (inc. reminders)
+sudo mkdir $ff_installed_folder/distribution
+echo '{"policies": {"DisableAppUpdate": true}}' | sudo tee $ff_installed_folder/distribution/policies.json
+
+
 
