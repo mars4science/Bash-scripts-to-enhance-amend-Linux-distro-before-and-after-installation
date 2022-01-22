9eb860e8c09cc0ebe349a2705cb34304f4cdd613
Alex Martian <alex4science4mars@gmail.comm>
2022-01-22 08:56:06 +0000

    move e_ject to global bashrc

--- after_login_config.sh
+++ after_login_config.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 
+# decided to edit system wide 
+# /etc/bash.bashrc instead of ~/.bashrc
+
+# ~/.bashrc contains lines that overwrite PS1 from /etc/bash.bashrc, so editing ~/.bashrc to change user prompt is needed 
 echo '' >> ~/.bashrc
-echo '# bash prompt, LM original and setting it' >> /home/$(id -u -n)/.bashrc
-echo '# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$' >> /home/$(id -u -n)/.bashrc
-echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' >> /home/$(id -u -n)/.bashrc
+echo '# bash prompt, LM original and setting it' >> ~/.bashrc
+echo '# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$' >> ~/.bashrc
+echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' >> ~/.bashrc
+echo '' | sudo tee --append /etc/bash.bashrc
+echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' | sudo tee --append /etc/bash.bashrc
+echo '' | sudo tee --append /etc/bash.bashrc
 
 # https://unix.stackexchange.com/questions/35508/eject-usb-drives-eject-command/83587
 # Not sure a function need to be exported each time of login, but count not find an asnwer to that in man bash and do not want to google now.
@@ -19,28 +26,28 @@ echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' >> /home/$(id -u -n)/.bashrc
 # Not sure a function need to be exported each time of login, but count not find an asnwer to that in man bash and do not want to google now. Therefore to be on the safe side below (adding to user profile) is expected to be Ok (multiple re-export did not output any errors):
 
 # ADDED: sometimes produced error at time of power-off, hypothesis is that umounting took longer than exit from eject, so added a cycle
-echo '' >> ~/.bashrc
-# echo 'e_ject() { dev=$(mount | grep --ignore-case $1 | awk --field-separator " " '\''{ FS = " " ; print $1 ; exit }'\'');2>/dev/null eject $dev;udisksctl power-off -b $dev; }; export -f e_ject' >> /home/$(id -u -n)/.bashrc
+echo '' | sudo tee --append /etc/bash.bashrc
+# echo 'e_ject() { dev=$(mount | grep --ignore-case $1 | awk --field-separator " " '\''{ FS = " " ; print $1 ; exit }'\'');2>/dev/null eject $dev;udisksctl power-off -b $dev; }; export -f e_ject' | sudo tee --append /etc/bash.bashrc
 # awk separator is space by default
 # for awk '' are needed cause $1 is awk notation, if "" than would be expanded by the shell
-echo 'e_ject() {' >> /home/$(id -u -n)/.bashrc
-echo '    dev=$(mount | grep --ignore-case "$1" | awk '\''{ print $1 }'\'')' >> /home/$(id -u -n)/.bashrc
-echo '    echo $dev | grep " " # check if space is present, then two or more lines were selected' >> /home/$(id -u -n)/.bashrc
-echo '    if [ $? -eq 0 ]; then echo "Two or more mounts matched, please pass more specific parameter"; return; fi' >> /home/$(id -u -n)/.bashrc
-echo '    if [ -z $dev ]; then echo "No mounts contaning phrase [$1] found"; return; fi' >> /home/$(id -u -n)/.bashrc
-echo '    2>/dev/null eject $dev;' >> /home/$(id -u -n)/.bashrc
-echo '    i=1' >> /home/$(id -u -n)/.bashrc
-echo '    for (( ; i < 10; i++ )); do ' >> /home/$(id -u -n)/.bashrc
-echo '        if [ -z $(mount | grep --ignore-case "$1" | awk '\''{ print $1 }'\'') ]; then' >> /home/$(id -u -n)/.bashrc
-echo '            udisksctl power-off -b $dev; ' >> /home/$(id -u -n)/.bashrc
-echo '            return' >> /home/$(id -u -n)/.bashrc
-echo '        fi' >> /home/$(id -u -n)/.bashrc
-echo '        sleep 1' >> /home/$(id -u -n)/.bashrc
-echo '    done' >> /home/$(id -u -n)/.bashrc
-echo '    echo "Takes long to unmount, had not attempted to power off"' >> /home/$(id -u -n)/.bashrc
-echo '}; export -f e_ject' >> /home/$(id -u -n)/.bashrc
+echo 'e_ject() {' | sudo tee --append /etc/bash.bashrc
+echo '    dev=$(mount | grep --ignore-case "$1" | awk '\''{ print $1 }'\'')' | sudo tee --append /etc/bash.bashrc
+echo '    echo $dev | grep " " # check if space is present, then two or more lines were selected' | sudo tee --append /etc/bash.bashrc
+echo '    if [ $? -eq 0 ]; then echo "Two or more mounts matched, please pass more specific parameter"; return; fi' | sudo tee --append /etc/bash.bashrc
+echo '    if [ -z $dev ]; then echo "No mounts contaning phrase [$1] found"; return; fi' | sudo tee --append /etc/bash.bashrc
+echo '    2>/dev/null eject $dev;' | sudo tee --append /etc/bash.bashrc
+echo '    i=1' | sudo tee --append /etc/bash.bashrc
+echo '    for (( ; i < 10; i++ )); do ' | sudo tee --append /etc/bash.bashrc
+echo '        if [ -z $(mount | grep --ignore-case "$1" | awk '\''{ print $1 }'\'') ]; then' | sudo tee --append /etc/bash.bashrc
+echo '            udisksctl power-off -b $dev; ' | sudo tee --append /etc/bash.bashrc
+echo '            return' | sudo tee --append /etc/bash.bashrc
+echo '        fi' | sudo tee --append /etc/bash.bashrc
+echo '        sleep 1' | sudo tee --append /etc/bash.bashrc
+echo '    done' | sudo tee --append /etc/bash.bashrc
+echo '    echo "Takes long to unmount, had not attempted to power off"' | sudo tee --append /etc/bash.bashrc
+echo '}; export -f e_ject' | sudo tee --append /etc/bash.bashrc
 
-echo '' >> ~/.bashrc
+echo '' | sudo tee --append /etc/bash.bashrc
 
 # does not work for liveUSB
 echo '' >> ~/.profile
