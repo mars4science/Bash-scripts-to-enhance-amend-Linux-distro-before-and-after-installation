353f7fe7ed381144682f2649c49b5a9348ffae9f
Alex Martian <alex4science4mars@gmail.com>
2022-01-22 19:01:23 +0300

    added check for ordinary user vs root in after_login_config.sh

--- after_login_config.sh
+++ after_login_config.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
 trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 
 # decided to edit system wide 
 # /etc/bash.bashrc instead of ~/.bashrc
 
 # ~/.bashrc contains lines that overwrite PS1 from /etc/bash.bashrc, so editing ~/.bashrc to change user prompt is needed 
-echo '' >> ~/.bashrc
-echo '# bash prompt, LM original and setting it' >> ~/.bashrc
-echo '# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$' >> ~/.bashrc
-echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' >> ~/.bashrc
+# if current user is root (e.g. chrooted duriong liveUSB creation), then ~ would exist, but not /home/$(id -u -n)
+if [ -e /home/$(id -u -n)/.bashrc ]; then
+    echo '' >> ~/.bashrc
+    echo '# bash prompt, LM original and setting it' >> ~/.bashrc
+    echo '# \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$' >> ~/.bashrc
+    echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' >> ~/.bashrc
+fi
 echo '' | sudo tee --append /etc/bash.bashrc
 echo 'PS1='\''\[\033[01;34m\]\w\[\033[00m\]\$ '\''' | sudo tee --append /etc/bash.bashrc
 echo '' | sudo tee --append /etc/bash.bashrc
@@ -50,11 +54,13 @@ echo '}; export -f e_ject' | sudo tee --append /etc/bash.bashrc
 echo '' | sudo tee --append /etc/bash.bashrc
 
 # does not work for liveUSB
-echo '' >> ~/.profile
-echo '# use RAM for cache' >> /home/$(id -u -n)/.profile
-# use "" to allow $id expand in echo
-echo "mount /home/$(id -u -n)/.cache" >> /home/$(id -u -n)/.profile
-echo '' >> /home/$(id -u -n)/.profile
+if [ -e /home/$(id -u -n)/.profile ]; then
+    echo '' >> ~/.profile
+    echo '# use RAM for cache' >> /home/$(id -u -n)/.profile
+    # use "" to allow $id expand in echo
+    echo "mount /home/$(id -u -n)/.cache" >> /home/$(id -u -n)/.profile
+    echo '' >> /home/$(id -u -n)/.profile
+fi
 
 exit
 
