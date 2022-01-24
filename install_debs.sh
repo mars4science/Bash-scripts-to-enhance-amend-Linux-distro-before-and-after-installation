cb7c1cfd8d8b78fa69c1aa1c8ed34a860a9e722d
Alex Martian <alex4science4mars@gmail.com>
2022-01-24 12:21:40 +0300

    changed call to apt_get from for each package to list of all packages to allow to output all errors at the end (new functionality of apt_get)

--- install_debs.sh
+++ install_debs.sh
@@ -2,6 +2,15 @@
 trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 
 # install all packages from local storage using apt_get.sh 
+# as apt_get got altered to output all errors at the end, developer changes this script to run apt_get only once, not for every package.
+# In fact the developed does not recall why longer code [1] was developed in the first place
+
+ls -1 "$(./apt_get.sh printpath)" | ./apt_get.sh -i
+exit
+
+===== previous version follows =====
+
+[1] 
 # -L to follow links, one may create a directory with links for specific install, edit default_local_debs line in apt_get.sh
 find -L "$(./apt_get.sh printpath)" -maxdepth 1 -mindepth 1 -type d -execdir bash -c 'basename $(ls -d1 "{}")' \; | { while read debs_path; do echo $debs_path | ./apt_get.sh -i; done; }
 
