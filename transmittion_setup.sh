6e83f9116026e684929a93acd67b10b6f9a1505f
Alex Martian <alex4science4mars@gmail.com>
2022-01-24 14:25:42 +0300

    fix adding mkdir if no folder for transmission settings in home

--- transmittion_setup.sh
+++ transmittion_setup.sh
@@ -6,7 +6,9 @@ if [ -e /home/$(id -u -n) ]; then
     # the json files originally had acces rights -rw-------, but copy via different users' accounts ids have to add access to others,
     # so (not tested yet) copy without attributes and set file mode bits later
     script_path_folder=$(dirname $(realpath $0))
-    cp --no-preserve=all "$script_path_folder"/settings/transmission/*.json /home/$(id -un)/.config/transmission
+    transmission_home_folder="/home/$(id -un)/.config/transmission"
+    if [ ! -d "$transmission_home_folder" ]; then mkdir --parents "$transmission_home_folder"; fi 
+    cp --no-preserve=all "$script_path_folder"/settings/transmission/*.json "$transmission_home_folder"
     sed --in-place 's`/mint/`/'$(id -un)'/`' /home/$(id -un)/.config/transmission/settings.json
     chmod a=,u=rw /home/$(id -un)/.config/transmission/*
 fi
