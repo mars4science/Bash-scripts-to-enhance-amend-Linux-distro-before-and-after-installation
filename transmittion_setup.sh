f64ad3f765f698ba2c7b8d22a963b8a8278fe076
Alex Martian <alex4science4mars@gmail.com>
2022-01-24 12:20:15 +0300

    changed path of settings to be copied from from specific folder to one relative to script location

--- transmittion_setup.sh
+++ transmittion_setup.sh
@@ -5,7 +5,8 @@
 if [ -e /home/$(id -u -n) ]; then
     # the json files originally had acces rights -rw-------, but copy via different users' accounts ids have to add access to others,
     # so (not tested yet) copy without attributes and set file mode bits later
-    cp --no-preserve=all /home/mint/Documents/Projects/Scripts/settings/transmission/*.json /home/$(id -un)/.config/transmission
+    script_path_folder=$(dirname $(realpath $0))
+    cp --no-preserve=all "$script_path_folder"/settings/transmission/*.json /home/$(id -un)/.config/transmission
     sed --in-place 's`/mint/`/'$(id -un)'/`' /home/$(id -un)/.config/transmission/settings.json
     chmod a=,u=rw /home/$(id -un)/.config/transmission/*
 fi
