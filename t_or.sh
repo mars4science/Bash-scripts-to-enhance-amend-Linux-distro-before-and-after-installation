3dcf7e12104cf24c08d0d57f4e1cb166bbbbd812
Alex Martian <alex4science4mars@gmail.com>
2022-02-20 16:38:05 +0300

    changed variable name that was used twice for different meanings that created error when one assignment was moved

--- t_or.sh
+++ t_or.sh
@@ -5,12 +5,12 @@ trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 # TODO think about hardcoded copyfrom_path
 
 run_path=/media/ramdrive
-source_path="$(get_software_path.sh)"/tor.tar.xz
+link_path="$(get_software_path.sh)"/tor.tar.xz
 
 # for "install" and "update" arguments
 source common_arguments_to_scripts.sh
 # help
-help_message="  Runs tor browser from $run_path (first extracts that application from archive expected to be located in $source_path).
+help_message="  Runs tor browser from $run_path (first extracts that application from archive expected to be located in $link_path).
   Usage: $script_name\n"
 display_help "$help_message$common_help"
 # ====== #
@@ -20,21 +20,21 @@ display_help "$help_message$common_help"
 # -a file True if file exists.; -h file True if file exists and is a symbolic link. -e file True if file exists.;
 # -a f is not negated properly as -a is also a binary and somehow binary takes precedence
 # see https://unix.stackexchange.com/questions/676608/bash-negation-of-a-file-exists-does-not-change-result-whereas-for-e-chang
-if [ ! -e "$source_path" ]; then
-    if [ ! -d $(dirname "$source_path") ]; then sudo mkdir $(dirname "$source_path"); fi
+if [ ! -e "$link_path" ]; then
+    if [ ! -d $(dirname "$link_path") ]; then sudo mkdir $(dirname "$link_path"); fi
     software_path_root=/media/$(id -un)/usb/LM_20.2
     copyfrom_path="$software_path_root/tor-browser-linux64-10.5.10_en-US.tar.xz"
     if [ ! -e "$copyfrom_path" ]; then echo >&2 "tor path $copyfrom_path not found, exiting with error"; exit 1; fi
-    sudo cp "$copyfrom_path" $(dirname "$source_path")
-    sudo ln -s $(dirname "$source_path")/$(basename "$copyfrom_path") $source_path
-    echo copied and linked tor to "$source_path", run script again to copy to RAM and start from there
+    sudo cp "$copyfrom_path" $(dirname "$link_path")
+    sudo ln -s $(dirname "$link_path")/$(basename "$copyfrom_path") $link_path
+    echo copied and linked tor to "$link_path", run script again to copy to RAM and start from there
     exit 0
 fi
 
 # delete previous run
 if [ -d $run_path/tor-browser_en-US ]; then rm --recursive $run_path/tor-browser_en-US; fi
 
-cp --dereference "$source_path" $run_path
+cp --dereference "$link_path" $run_path
 7z e -o"$run_path" "$run_path"/tor.tar.xz
 rm "$run_path"/tor.tar.xz
 # 7z does not restore permissions
