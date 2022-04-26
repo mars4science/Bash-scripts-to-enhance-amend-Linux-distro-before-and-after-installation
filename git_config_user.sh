5e6a024c0df3dfbdfbbd1f70c5ab3e192d77061b
Alex Martian <alex4science4mars@gmail.com>
2022-04-26 13:35:10 +0300

    script to add user name and e-mail git_config_user.sh created, written to be run manually after repo cloning

---
+++ git_config_user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# trap 'err=$?; echo >&2 "Exiting on error $err"; sleep 10; exit $err' ERR
+
+# change hardly visible red to yellow in git output. To see all available places to change color, type git config --global color. and tab
+# list of colors in "git help config"
+git config --global user.email alex4science4mars@gmail.com
+git config --global user.name Alex Martian
