a56d7c346a52324c7082eabeb8d3e4593db9cd7b
Alex Martian <alex4science4mars@gmail.com>
2022-05-04 19:25:41 +0300

    added custom key bindings for X1G6 trackpoint fix and screen scaling fix

--- dconf_config.sh
+++ dconf_config.sh
@@ -47,7 +47,7 @@ gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-battery-
 
 # See [1] of _readme.md, also for some reason command (for binding) that have $(xrandr do not work, only specific with e.g. eDP-1,
 # therefore changed script code to make shell files and bind to them - it resulted in being able to use keys to rotate system's display
-gsettings set org.cinnamon.desktop.keybindings custom-list "['custom0', 'custom1', 'custom2', 'custom3', 'custom4', '__dummy__']"
+gsettings set org.cinnamon.desktop.keybindings custom-list "['custom0', 'custom1', 'custom2', 'custom3', 'custom4', 'custom5', 'custom6', '__dummy__']"
 # or dconf write /org/cinnamon/desktop/keybindings/custom-list "['custom0', 'custom1', 'custom2', '__dummy__']"
 echo 'xrandr --output $(xrandr -q|grep -v disconnected|grep connected|awk '\''{print $1}'\'') --rotate normal' | sudo tee $(get_install_path.sh)/display_rotate_normal.sh
 echo 'xrandr --output $(xrandr -q|grep -v disconnected|grep connected|awk '\''{print $1}'\'') --rotate left' | sudo tee $(get_install_path.sh)/display_rotate_left.sh
@@ -76,8 +76,18 @@ dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom4/name "'
 dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom4/binding "['<Primary>u']"
 dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom4/command "'pactl set-sink-volume @DEFAULT_SINK@ +25%'"
 
+# fix TrackPoint issue om carbon X1 gen 6
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom5/name "'TrackPoint X1G6 fix'"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom5/binding "['<Super><Alt>t']"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom5/command "'/lib/systemd/system-sleep/trackpoint_reset key'"
+
+# set custom screen scale
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom6/name "'Screen scale'"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom6/binding "['<Super><Alt>s']"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom6/command "'/lib/systemd/system-sleep/scaling_factor key'"
+
 # to activate bindings (for some reason do not work just from populating dconf database from above commands), different order of items in the list
-gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__', 'custom0', 'custom1', 'custom2', 'custom3', 'custom4']"
+gsettings set org.cinnamon.desktop.keybindings custom-list "['__dummy__', 'custom0', 'custom1', 'custom2', 'custom3', 'custom4', 'custom5', 'custom6']"
 # or dconf write /org/cinnamon/desktop/keybindings/custom-list "['__dummy__', 'custom0', 'custom1', 'custom2', 'custom3', 'custom4']"
 
 gsettings set org.nemo.preferences show-hidden-files true
