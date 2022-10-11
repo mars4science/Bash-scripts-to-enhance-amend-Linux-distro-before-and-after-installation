6f2f244202bb67279b68e300c88079ad60c11485
Alex Martian <alex4science4mars@gmail.com>
2022-10-11 04:58:54 +0300

    changed upower low and critical levels - maybe will change nothing in the way system works

--- upower_battery_config.sh
+++ upower_battery_config.sh
@@ -8,8 +8,7 @@ conf_file=/etc/UPower/UPower.conf
 # I have not noticed any actions on Low and Critical, Action works but only when percentage is set to lower than Low and Critical, as
 # > If any value is invalid, or not in descending order, the defaults
 # > will be used.
-# Therefore set Low and Critical to 95 and 90 respectfully.
 sudo sed -E --in-place=bak 's/^UsePercentageForPolicy=false/UsePercentageForPolicy=true/' $conf_file
-sudo sed -E --in-place=bak 's/^PercentageLow=[0-9]{1,}/PercentageLow=95/' $conf_file
-sudo sed -E --in-place=bak 's/^PercentageCritical=[0-9]{1,}/PercentageCritical=90/' $conf_file
+sudo sed -E --in-place=bak 's/^PercentageLow=[0-9]{1,}/PercentageLow=20/' $conf_file
+sudo sed -E --in-place=bak 's/^PercentageCritical=[0-9]{1,}/PercentageCritical=15/' $conf_file
 sudo sed -E --in-place=bak 's/^PercentageAction=[0-9]{1,}/PercentageAction=10/' $conf_file
