ccf38204de8227aeb556150df2af6f5467610921
Alex Martian <alex4science4mars@gmail.com>
2022-10-13 11:30:45 +0300

    changed location to put youtube-dl, added reason in comments, replaced hardcoded location with get_install_path.sh for yt-dlp

--- setup_youtube-downloaders.sh
+++ setup_youtube-downloaders.sh
@@ -4,16 +4,17 @@
 
 # alternative to youtube-dl, have read (and saw) work fast when youtube-dl is slow to download
 if [ "x${software_path_root}" = "x" ] ; then software_path_root=/media/$(id -un)/usb/LM_20.2 ; fi
-sudo cp "$software_path_root"/bin/yt-dlp /usr/local/bin
-sudo chmod +x /usr/local/bin/yt-dlp
+sudo cp --no-clobber "$software_path_root"/bin/yt-dlp $(get_install_path.sh)
+sudo chmod +x $(get_install_path.sh)/yt-dlp
 
 # to setup proper python for youtube-dl
 sudo ln -s /usr/bin/python3 /usr/local/bin/python
 
 # 2021/12/12 looks like LM 20.2 distro I use contains youtube-dl
-sudo cp "$software_path_root"/bin/youtube-dl /usr/bin # removed /local, not overwrite original from distro, after replacement there is next line error no more
-# youtube-dl: error: youtube-dl's self-update mechanism is disabled on Debian.
-sudo chmod +x /usr/bin/youtube-dl # just in case there was no youtube-dl in that location
+sudo cp --no-clobber "$software_path_root"/bin/youtube-dl $(get_install_path.sh)
+# in /usr/bin expected to have original youtube-dl from distro, that gives error ("youtube-dl: error: youtube-dl's self-update mechanism is disabled on Debian.") when user tries to update; but in $PATH /usr/local/bin is searched before /usr/bin so as far as $(get_install_path.sh) points to /usr/local/bin one which is copied is called, not original from distro
+
+sudo chmod +x $(get_install_path.sh)/youtube-dl
 
 # to make it work immediately w/out bash restart
 # not sure it is needed and in that form (maybe -d python?)
@@ -28,6 +29,6 @@ sudo chmod +x /usr/bin/youtube-dl # just in case there was no youtube-dl in that
 # would update if device is connected to the internet
 # youtube-dl: error: youtube-dl's self-update mechanism is disabled on Debian.
 echo next is trying to update youtube-dl, script developer has assumed it needs internet
-sudo youtube-dl --update # even if outputs "ERROR: can't find the current version. Please try again later.", echo $? still 0, so result is seen manual way:
-echo if no errors after \"next is trying\" line then updated
+printf "%s" "  " && sudo youtube-dl --update # even if outputs "ERROR: can't find the current version. Please try again later.", echo $? still 0, so result is seen manual way:
+echo '  if no errors after "next is trying" line then updated'
 
