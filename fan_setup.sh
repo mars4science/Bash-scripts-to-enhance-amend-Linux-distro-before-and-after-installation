03f5805545a065c42ff302808ec0331729af3984
Alex Martian <alex4science4mars@gmail.com>
2022-10-13 11:32:19 +0300

    added argument not to replace when copying

--- fan_setup.sh
+++ fan_setup.sh
@@ -4,7 +4,7 @@ trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 
 if [ "x${software_path_root}" = "x" ] ; then software_path_root=/media/$(id -un)/usb/LM_20.2 ; fi
 
-sudo cp "$software_path_root"/bin/stopfan $(get_install_path.sh)
+sudo cp --no-clobber "$software_path_root"/bin/stopfan $(get_install_path.sh)
 # sudo chown root /usr/bin/stopfan
 # will be owned by root after copy anyway
 # +xs execute as owner (root)
