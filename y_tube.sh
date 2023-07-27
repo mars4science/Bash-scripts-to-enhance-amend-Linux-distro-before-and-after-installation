4cd27475802be97fbd74c8b40e7e58e63260337f
Alex Martian <alex4science4mars@gmail.comm>
2023-07-27 18:18:33 +0000

    y_tube: added order of preference for video/audio formats

--- y_tube.sh
+++ y_tube.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 2023/07 List produced by `yt-dlp --list-subs URL` contained certain subtitles but on download they did not contain useful info, therefore adding language codes encountered and filtering out empty later
-params=" --write-info-json --write-description --write-auto-sub --sub-langs en-en,en,fr,fr-en,fr-en-US,de,de-en,de-en-US,ru,ru-en,ru-en-US,uk,uk-en,uk-en-US " # from 2023/07 translations from English are codes as ??-en
+# "--format-sort ext" helped to select "--prefer-free-formats" over "quality" (see "Filtering Formats" in README of yt-dlp)
+params=" --prefer-free-formats --format-sort ext --write-info-json --write-description --write-auto-sub --sub-langs en-en,en,fr,fr-en,fr-en-US,de,de-en,de-en-US,ru,ru-en,ru-en-US,uk,uk-en,uk-en-US " # from 2023/07 translations from English are codes as ??-en
 
 # for install and update arguments
 source "$(dirname "$(realpath "$0")")"/common_arguments_to_scripts.sh
@@ -16,15 +17,19 @@ display_help "$help_message$common_help"
 params_add=" -- "
 if [ $# -eq 2 ]; then
     case $1 in
-        "h" ) params_add=" -f 271+251 -- ";;
-# [1] decided not to check formats before try to download to save resources on accessing youtube twice ... yt-dlp --list-formats $2 | grep 271 ;;
-        "l" ) params_add=" -f 22 -- " ;;
+# see yt-dlp README for syntax, h looks for up to 1200p no less than 800p horizontal, trying for <=30 fps 1) merged, then 2) separate video+audio then drop fps constraint
+        "h" ) params_add=' -f best[height<=1200][height>=800][fps<=30]/bv[height<=1200][height>=1800][fps<=30]+ba/bv[height<=1200][height>=1800]+ba/bv[height<=1200]+ba -- ';;
+# h looks for up to 800p  no less than 600p horizontal, trying for <=30 fps 1) merged, then 2) separate video+audio then 3) drop minimum horizontal constraint
+        "l" ) params_add=' -f best[height<=800][height>=600][fps<=30]/bv[height<=800][height>=600][fps<=30]+ba/bv[height<=800][fps<=30]+ba -- ';;
     esac
+#    set -x # bash option "Print commands and their arguments as they are executed"
     yt-dlp $params $params_add $2
+#    set +x
     if [ $? -eq 1 ];then 
         echo "exit status 1 (fail?) after code to try to download, hypothesis is that selected format is not available; "
-        echo "next code to try without format arguments"
-        yt-dlp $params $2
+#        echo "next code to try without format arguments"
+#        yt-dlp $params $2
+        exit 1
     fi
     URL=$2
 else
@@ -49,13 +54,13 @@ for f in *"$video_id"*vtt  ; do
 done
 
 # rename subtitle files for loading by mpv (e.g. suffix de-en.vtt to de.vtt)
-for f in *"$video_id"*-en.vtt  ; do
+for f in *"$video_id"*-en.vtt ; do
     f1="${f/-en.vtt/.vtt}"
     if [ ! -e "${f1}" ] && [ -e "${f}" ] ; then
         mv "${f}" "${f1}"
     fi
 done
-for f in *"$video_id"*-en-US.vtt  ; do
+for f in *"$video_id"*-en-US.vtt ; do
     f1="${f/-en-US.vtt/.vtt}"
     if [ ! -e "${f1}" ] && [ -e "${f}" ] ; then
         mv "${f}" "${f1}"
