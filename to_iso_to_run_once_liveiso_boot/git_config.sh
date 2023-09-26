ab30f08a336ee9807ca4d8110bbd78e9f7adf385
Alex <alex4science4mars@gmail.com>
2023-09-26 23:31:53 +0000

    additonal git setup enhancements

--- to_iso_to_run_once_liveiso_boot/git_config.sh
+++ to_iso_to_run_once_liveiso_boot/git_config.sh
@@ -2,6 +2,11 @@
 # trap 'err=$?; echo >&2 "Exiting on error $err"; sleep 10; exit $err' ERR
 
 # change hardly visible red to yellow in git output. To see all available places to change color, type git config --global color. and tab
-# list of colors in "git help config"
+# for list of colors and ways to use them see "git help config"
+# for list of options to set use "git help --config"
 git config --global color.status.changed yellow
 git config --global color.diff.old yellow
+git config --global color.interactive.error "red white" # "foreground background"
+git config --global color.push.error "red white"
+git config --global color.remote.error "red white"
+
