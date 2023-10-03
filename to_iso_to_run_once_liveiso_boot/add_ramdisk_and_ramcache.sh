f7096e71cca5ea7c17c90b01b383ad921eb129da
Alex Martian <alex4science4mars@gmail.com>
2023-10-03 13:55:11 +0000

    added check before append: if /etc/fstab entries are already there

--- to_iso_to_run_once_liveiso_boot/add_ramdisk_and_ramcache.sh
+++ to_iso_to_run_once_liveiso_boot/add_ramdisk_and_ramcache.sh
@@ -7,15 +7,25 @@ trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 # ! for zram, after some files deleted there, looks like needed to run manually `sudo fstrim /mountpoint` to discard memory (unused blocks)
 
 # new way for ramdisk
-echo 'tmpfs /media/ramdisk tmpfs size=100%,x-mount.mkdir,noatime 0 0' | sudo tee --append /etc/fstab
+grep 'tmpfs /media/ramdisk' /etc/fstab
+if [ $? -ne 0 ]; then
+    echo 'tmpfs /media/ramdisk tmpfs size=100%,x-mount.mkdir,noatime 0 0' | sudo tee --append /etc/fstab
+fi
+
 # adding zramdisk via mount helper resulted in disk visible after boot, but not opening, so comment out /dev/zram0
 # echo '/dev/zram0 /media/zramdisk zramdisk x-systemd.automount,x-mount.mkdir,discard,noatime,dev,suid,exec 0 0' | sudo tee --append /etc/fstab
-echo 'tmpfs /home/root/.cache tmpfs size=1%,noatime 0 0' | sudo tee --append /etc/fstab
+grep 'tmpfs /home/root/.cache' /etc/fstab
+if [ $? -ne 0 ]; then
+    echo 'tmpfs /home/root/.cache tmpfs size=1%,noatime 0 0' | sudo tee --append /etc/fstab
+fi
 
 # swap in zram in case will be needed.
 # as zram device is not available on early during boot, fstab entry does not result in automatic swap activation
 # with this entry in fstab swap can be started either `swapon /dev/zram1` or `swapon --all`
-echo '/dev/zram1 none swap 0 0' | sudo tee --append /etc/fstab
+grep '/dev/zram1' /etc/fstab
+if [ $? -ne 0 ]; then
+    echo '/dev/zram1 none swap 0 0' | sudo tee --append /etc/fstab
+fi
 
 # the below moved to /boot/init_custom.sh (made via run_at_boot_config.sh) as home is encrypted and links need to be added at boot,
 # might be ok to add via systemd,
@@ -25,14 +35,17 @@ echo '/dev/zram1 none swap 0 0' | sudo tee --append /etc/fstab
 # if home is encrypted, that entry is fstab would be overwritten when 
 # encryptfs is initialized: systemd: "Reached target Local Encrypted Volumes"
 # therefore using noauto in /etc//fstab and mounting in user .profile file.
-echo 'tmpfs /home/'$(id -u -n)'/.cache tmpfs size=5%,noauto,user,noatime 0 0' | sudo tee --append /etc/fstab
+grep "tmpfs /home/$(id -u -n)/.cache" /etc/fstab
+if [ $? -ne 0 ]; then
+    echo 'tmpfs /home/'$(id -u -n)'/.cache tmpfs size=5%,noauto,user,noatime 0 0' | sudo tee --append /etc/fstab
+fi
 
 # add noatime, then access time not changed on access, saving SSD, even if timestamp of access is older then 1 day (man mount
 # "In addition, since Linux 2.6.30, the file's last access time is always updated if it is more than 1 day old."- than it is inddeed for relatime only
 sudo sed --in-place --regexp-extended -- 's/errors=remount-ro/errors=remount-ro,noatime/' /etc/fstab
 
-# ram disk is needed for some install scripts later
-# also developer found out during liveUSB boot /etc/fstab file in squashfs gets overshadowed,
+# ram disk was? needed for some install scripts later
+# also developer found out that during liveUSB boot /etc/fstab file in squashfs gets overshadowed,
 # so next line is "user specfic" (to be run after boot)
 sudo mount /media/ramdisk
 # sudo mount /media/zramdisk, now mounted in add_zram.sh
