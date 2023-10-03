801e0d673a3a9aa718c9360cafd41d6b0d7d3da7
Alex Martian <alex4science4mars@gmail.com>
2023-10-03 13:59:29 +0000

    edited regex for sed to make not to replace if already done

--- disable_swap.sh
+++ disable_swap.sh
@@ -2,7 +2,7 @@
 trap 'err=$?; echo >&2 "Exiting on error $err"; exit $err' ERR
 
 # ` used instead of usual / in replace s as / is part of string to be replaced
-sudo sed --in-place=bak 's`/swap`#/swap`' /etc/fstab
+sudo sed --in-place=bak 's`/^swap`#/swap`' /etc/fstab
 sudo swapoff --all
 # added to prevent error in case of liveUSB
 if [ -e /swapfile ]; then sudo rm /swapfile; fi
