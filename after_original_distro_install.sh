e6d00a60060257a5eeebe0309421a51185476440
Alex Martian <alex4science4mars@gmail.com>
2023-10-04 11:05:26 +0000

    added TODOs to make scripts safe for re-run on already changed by them distro (after checking and fixing the others)

--- after_original_distro_install.sh
+++ after_original_distro_install.sh
@@ -86,7 +86,7 @@ $dir_name/to_iso_to_run_once_liveiso_boot/add_ramdisk_and_ramcache.sh
 
 $dir_name/disable_swap.sh
 $dir_name/display_backlight_control_setup.sh
-$dir_name/bash_functions_and_other_config.sh
+$dir_name/bash_functions_and_other_config.sh # TODO: fix of: will add again and again if run again
 
 # enable packet forwarding for IPv4, IPv6
 $dir_name/networking_tweaks.sh
@@ -101,25 +101,26 @@ echo "===== This line is after install code, next in a few seconds going to cont
 sleep 5 # pause n seconds
 
 # to be run after programs install
-$dir_name/tlp_config_battery.sh
-$dir_name/upower_battery_config.sh # inc. change critial battery level to take action
-$dir_name/change_default_apps_for_multimedia_files.sh
+$dir_name/tlp_config_battery.sh # TODO: fix of: crontab again
+$dir_name/upower_battery_config.sh # includes change critial battery level to take action
+$dir_name/change_default_apps_for_multimedia_files.sh # TODO: check whether safe for re-run
 $dir_name/mpv_config.sh
 
 # install gecko and mono for wine, before was separate script to run both with "source" command so that error installing 1st resulted in skipping 2nd
 $dir_name/install_wine-gecko.sh
 $dir_name/install_wine-mono.sh
 
-# would try to update from Internet
+# might try to update from Internet
 $dir_name/setup_youtube-downloaders.sh
+
 $dir_name/y_tube.sh install
 
 # TODO add config for thinkfan 
-# TODO check what thinkfan do (how interacts with manual control, e.g. via `stopfan`)as it is likely to be installed 
+# TODO check what thinkfan do (how interacts with manual control, e.g. via `stopfan`) as it is likely to be installed
 
 # add applets/desklets software to Cinnamon
 $dir_name/cinnamon_add_software.sh
-$dir_name/cinnamon_config.sh
+$dir_name/cinnamon_config.sh # TODO: make re-runnable
 
 # copy additional color profiles
 sudo cp "${software_path_root}/color-profiles/"* /usr/share/color/icc/colord
@@ -148,14 +149,14 @@ if [ $running_system = "true" ]; then
         echo "liveUSB kind of detected, would not tweak boot as not tested for persitent liveUSB"
     else
         # add entry to start in text mode (level 3) and timeout delay change
-        # TODO try to make work fot liveUSB
+        # TODO try to make it work for liveUSB
         $dir_name/grub_config.sh
         # to be run at the end of setup as renames /var directory which would effect some previous steps
         $dir_name/run_at_boot_config.sh
     fi
 
     $dir_name/to_iso_to_run_once_liveiso_boot/libvirt_access_rights.sh # for liveUSB running `--append --groups libvirt $user_name` AFAIK has no effect (need reboot or at least relogin), but ACL in /media might be useful
-    $dir_name/to_iso_to_run_once_liveiso_boot/user_specific.sh
+    $dir_name/to_iso_to_run_once_liveiso_boot/user_specific.sh # TODO: check whether safe for re-run
 
     # to leave terminal open with interactive bash if started from GUI
     # ps output list processes' paths as called (relative), so use "$0" 
