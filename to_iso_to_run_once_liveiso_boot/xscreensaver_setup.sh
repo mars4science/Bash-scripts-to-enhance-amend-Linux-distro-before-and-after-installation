ff5f62d35ed65966a9e6ff3bc1113c03b1271314
Alex Martian <alex4science4mars@gmail.com>
2023-10-06 14:16:20 +0000

    incorrect comment fix

--- to_iso_to_run_once_liveiso_boot/xscreensaver_setup.sh
+++ to_iso_to_run_once_liveiso_boot/xscreensaver_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# copy configs that set upload speed limit and seeding limits.
+# copy xscreensaver config file
 
 # if current user is root (e.g. chrooted duriong liveUSB creation), then ~ would exist, but not /home/$(id -u -n)
 if [ "x${software_path_root}" = "x" ] ; then software_path_root=/media/$(id -un)/usb/LM ; fi
