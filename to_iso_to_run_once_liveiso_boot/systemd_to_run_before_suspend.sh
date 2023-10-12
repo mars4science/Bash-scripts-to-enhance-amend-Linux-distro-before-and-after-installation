02b62723a75e867eafd1e67afcf2cf8b749ee305
Alex Martian <alex4science4mars@gmail.com>
2023-10-12 20:29:23 +0000

    fix: xscreensaver not starting fully before suspend

--- to_iso_to_run_once_liveiso_boot/systemd_to_run_before_suspend.sh
+++ to_iso_to_run_once_liveiso_boot/systemd_to_run_before_suspend.sh
@@ -3,3 +3,4 @@
 # start xscreensaver before suspend to prevent last screen to be seen after resume
 # running as user needed because seems `xscreensaver` needs .Xauthority to run screensaver
 sudo --user=mint xscreensaver-command --lock
+sudo --user=mint xscreensaver-command --lock # in one setting noted running just one time have been resulting in last screen flashing after resume, maybe simple `sleep 1` consistently achieves same
