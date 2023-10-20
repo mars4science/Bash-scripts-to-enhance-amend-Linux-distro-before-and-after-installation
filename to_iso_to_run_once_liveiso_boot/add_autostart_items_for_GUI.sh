4f16d76bb6c4858eaa569b66621a88ece504c2c7
Alex Martian <alex4science4mars@gmail.com>
2023-10-20 22:32:19 +0000

    moved changing fonts scaling to fonts_scaling.sh to be run as Cinnamon autostart desktop entry

--- to_iso_to_run_once_liveiso_boot/add_autostart_items_for_GUI.sh
+++ to_iso_to_run_once_liveiso_boot/add_autostart_items_for_GUI.sh
@@ -3,6 +3,9 @@
 autostart_dir=/home/$(id -u -n)/.config/autostart
 mkdir --parents $autostart_dir
 
+# Note: 
+# For `Exec` using '~' for home resulted in no expected results, so seems absolute paths needed
+
 # ----- BOOKMARKS ----- #
 
 # file created by GUI had Name[en_US] but no Name w/out locale, so in other system language interface boot it was not run
@@ -34,4 +37,20 @@ NoDisplay=false
 Hidden=false
 X-GNOME-Autostart-Delay=0
 EOF
+
+# set scaling of fonts in Cinnamon based on display's resolution and pixels density (and guesswork about overall monitor scaling factor as still have not found a way to read it by shell code)
+tee $autostart_dir/fonts.scaling.set.desktop << EOF
+[Desktop Entry]
+Name=fonts.scaling.set
+Comment=No description
+Type=Application
+Exec=liveiso_path_scripts_root/fonts_scaling.sh
+X-GNOME-Autostart-enabled=true
+NoDisplay=false
+Hidden=false
+X-GNOME-Autostart-Delay=0
+EOF
+
+# Seems to be no need to make desktop files executable in autostart, works w/out it but just in case:
+chmod a+x $autostart_dir/*
 
