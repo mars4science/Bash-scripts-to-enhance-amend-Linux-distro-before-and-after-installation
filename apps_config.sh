8f39d54be6a9b2157369564ebcfc7027c97c114b
Alex Martian <alex4science4mars@gmail.com>
2023-10-25 00:29:17 +0000

    improved mpv options setup

--- apps_config.sh
+++ apps_config.sh
@@ -9,55 +9,70 @@ else
     # overwrite previous settings (no --append) as script may be run not one time
     file_fully_qualified_name="${app_conf_folder}"/mpv.conf
     file_contents='hwdec=vaapi
-    sub-font-size=45
-    sub-color=0.0/1.0/1.0/0.2
-    sub-border-size=5
-    sub-border-color=0.0/0.0/0.0/0.3
-    sub-pos=70
-    # older scaletempo might result in cleaner dialog at reduced speed
-    af=scaletempo
-    # increase range of audio enabled speeds from default 0.25 to 4.0
-    # af=scaletempo2=min-speed=0.04:max-speed=25
-    fullscreen=yes'
+
+sub-font-size=45
+sub-color=0.0/1.0/1.0/0.2
+sub-border-size=5
+sub-border-color=0.0/0.0/0.0/0.3
+sub-pos=70
+
+no-audio-display # to disable displaying of cover art when playing audio files (e.g. works for mp3)
+
+# Options:
+#  scale                          Float (0.01 to any) (default: 1.000)
+#  stride                         Float (0.01 to any) (default: 60.000)
+#  overlap                        Float (0 to 1) (default: 0.200)
+#  search                         Float (0 to any) (default: 14.000)
+#  speed                          Choices: pitch tempo none both (default: tempo)
+af=scaletempo # older scaletempo might result in cleaner dialog at reduced speed
+
+# Options:
+#  search-interval                Float (1 to 1000) (default: 30.000)
+#  window-size                    Float (1 to 1000) (default: 20.000)
+#  min-speed                      Float (0 to 3.4028234663853e+38) (default: 0.250)
+#  max-speed                      Float (0 to 3.4028234663853e+38) (default: 4.000)
+# af=scaletempo2=min-speed=0.04:max-speed=25 # increase range of audio enabled speeds from default 0.25 to 4.0
+
+fullscreen=yes'
     echo "${file_contents}" | 1>/dev/null sudo tee "${file_fully_qualified_name}"
 
-    file_fully_qualified_name="${app_conf_folder}/input.conf"
-    file_contents='# increase subtitle font size
-    ALT+k add sub-scale +0.1
-    # decrease subtitle font size
-    ALT+j add sub-scale -0.1
+file_fully_qualified_name="${app_conf_folder}/input.conf"
+file_contents='# increase subtitle font size
+ALT+k add sub-scale +0.1
+# decrease subtitle font size
+ALT+j add sub-scale -0.1
 
-    ## Seek units are in seconds, but note that these are limited by keyframes
-    RIGHT no-osd seek  3 exact
-    LEFT  no-osd seek -1 exact
-    #UP    seek  60
-    #DOWN  seek -60
-    # Do smaller, always exact (non-keyframe-limited), seeks with shift.
-    # Don'\''t show them on the OSD (no-osd).
-    Shift+RIGHT no-osd seek  5 exact
-    Shift+LEFT  no-osd seek -5 exact
-    Shift+UP    no-osd seek  -60 exact
-    Shift+DOWN no-osd seek 60 exact
+## Seek units are in seconds, but note that these are limited by keyframes
+RIGHT no-osd seek  3 exact
+LEFT  no-osd seek -1 exact
+#UP    seek  60
+#DOWN  seek -60
+# Do smaller, always exact (non-keyframe-limited), seeks with shift.
+# Don'\''t show them on the OSD (no-osd).
+Shift+RIGHT no-osd seek  5 exact
+Shift+LEFT  no-osd seek -5 exact
+Shift+UP    no-osd seek  -60 exact
+Shift+DOWN no-osd seek 60 exact
 
-    / quit # set / key to quit player
-    a cycle audio-pitch-correction # set "a" key to enable/disable scaletempo/scaletempo2 pitch correction for sound
+/ quit # set / key to quit player
+a cycle audio-pitch-correction # set "a" key to enable/disable scaletempo/scaletempo2 pitch correction for sound
 
-    # various combinations with "add sub-color" to gradually change part of RGB/transparency failed to produce desired result
-    ALT+3 set sub-color 1.0/0.0/0.0/0.7
-    ALT+4 set sub-color 0.0/1.0/0.0/0.7
-    ALT+5 set sub-color 0.0/0.0/1.0/0.7
-    ALT+6 set sub-color 1.0/1.0/0.0/0.7
-    ALT+7 set sub-color 0.0/1.0/1.0/0.7
-    ALT+8 set sub-color 1.0/0.0/1.0/0.7
-    ALT+9 set sub-color 1.0/1.0/1.0/0.7
+# various combinations with "add sub-color" to gradually change part of RGB/transparency failed to produce desired result
+ALT+3 set sub-color 1.0/0.0/0.0/0.7
+ALT+4 set sub-color 0.0/1.0/0.0/0.7
+ALT+5 set sub-color 0.0/0.0/1.0/0.7
+ALT+6 set sub-color 1.0/1.0/0.0/0.7
+ALT+7 set sub-color 0.0/1.0/1.0/0.7
+ALT+8 set sub-color 1.0/0.0/1.0/0.7
+ALT+9 set sub-color 1.0/1.0/1.0/0.7
 
-    ALT+# set sub-color 1.0/0.0/0.0/0.2
-    ALT+$ set sub-color 0.0/1.0/0.0/0.2
-    ALT+% set sub-color 0.0/0.0/1.0/0.2
-    ALT+^ set sub-color 1.0/1.0/0.0/0.2
-    ALT+& set sub-color 0.0/1.0/1.0/0.2
-    ALT+* set sub-color 1.0/0.0/1.0/0.2
-    ALT+( set sub-color 1.0/1.0/1.0/0.2'
+ALT+# set sub-color 1.0/0.0/0.0/0.2
+ALT+$ set sub-color 0.0/1.0/0.0/0.2
+ALT+% set sub-color 0.0/0.0/1.0/0.2
+ALT+^ set sub-color 1.0/1.0/0.0/0.2
+ALT+& set sub-color 0.0/1.0/1.0/0.2
+ALT+* set sub-color 1.0/0.0/1.0/0.2
+ALT+( set sub-color 1.0/1.0/1.0/0.2'
     echo "${file_contents}" | 1>/dev/null sudo tee "${file_fully_qualified_name}"
 fi
 
