62a66f7d34384740c90789ba853e3da822469070
Alex Martian <alex4science4mars@gmail.com>
2023-10-26 01:40:08 +0000

    added code to make kiwix desktop file to be independent of appimage version

--- binaries_install.sh
+++ binaries_install.sh
@@ -4,12 +4,12 @@
 
 if [ "x${software_path_root}" = "x" ] ; then software_path_root=/media/$(id -un)/usb/LM ; fi
 
-sudo cp --no-clobber "$software_path_root"/bin/the_rest/* "$(get_install_path.sh)"
-# expected to have exec bit set in "$software_path_root", not setting here
+sudo cp --no-clobber "$software_path_root"/bin/the_rest/* "$(get_install_path.sh)" # expected to have exec bit set in "$software_path_root", not setting here
 
-# app apps in form of AppImage to disk and menu (to list of All Applications)
+# copy apps in form of AppImage to disk and menu (to list of All Applications)
 sudo cp --no-clobber "$software_path_root"/bin/appimages/* "$(get_install_path.sh)"
 sudo cp --no-clobber "$software_path_root"/bin/desktops/* /usr/share/applications
 sudo cp --no-clobber "$software_path_root"/bin/icons/* /usr/share/pixmaps
 
+sudo ln --relative --symbolic "$(get_install_path.sh)"/kiwix*.appimage "$(get_install_path.sh)"/kiwix.appimage # for desktop file to be independent of kiwix version
 
