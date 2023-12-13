5786cefe3ae2323d14480f5f5663eefb7521beeb
Alex Martian <alex4science4mars@gmail.com>
2023-12-13 04:25:42 +0000

    changed actions to activate xscreensaver during suspend: now blanking screen and initiating reactivation instead of locking twice

--- to_iso_to_run_once_liveiso_boot/systemd_to_run_before_suspend.sh
+++ to_iso_to_run_once_liveiso_boot/systemd_to_run_before_suspend.sh
@@ -2,5 +2,17 @@
 
 # start xscreensaver before suspend to prevent last screen to be seen after resume
 # running as user needed because seems `xscreensaver` needs .Xauthority to run screensaver
-sudo --user=mint xscreensaver-command --lock
-sudo --user=mint xscreensaver-command --lock # in one setting noted running just one time have been resulting in last screen flashing after resume, maybe simple `sleep 1` consistently achieves same
+# using --suspend and then --deactivate instead of --lock as --lock results in blinking on display of the underlining image immediately after xscreensaver already activated and started drawing (and that resulted in sometimes blinking of the image after resume); --deactivate added to give user after resume a clue that xscreensaver is running, not blank display
+sudo --user=mint sh -c '
+if [ -z "$(xscreensaver-command --time | grep locked)" ]; then
+    xscreensaver-command --suspend
+    xscreensaver-command --deactivate
+else
+    echo -e "\n Already locked"
+    return 0; fi
+
+if [ -n "$(xscreensaver-command --time | grep locked)" ]; then
+    return 0
+else
+    return 1
+fi'
