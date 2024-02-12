7f581375e4826a3f0969f43334c9b6f1109864bc
Alex Martian <alex4science4mars@gmail.com>
2024-02-12 12:26:23 +0000

    reduced frequency of memory checks by crontab as earlyoom is going to be added to free RAM automatically

--- to_iso_to_run_once_liveiso_boot/memory_notify_config.sh
+++ to_iso_to_run_once_liveiso_boot/memory_notify_config.sh
@@ -9,7 +9,7 @@ trap 'err=$?; echo >&2 "  ERROR: Exiting $0 on error $err"; exit $err' ERR
 # * * * * * if [ $(free -wm | awk ' /^Mem:/ { print $8 } ') -le 500 ]; then XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send 'Short on memory, 500 Mb available left.'; fi
 
 # changed priority to normal otherwise notifications vidget does not list critical type and they cannot be dismissed all at once
-(crontab -l 2>/dev/null; echo '* * * * * if [ $(free -wm | awk '\''/^Mem:/ { print $8 }'\'') -le 500 ]; then XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u normal '\''Available memory: 500 Mb only!'\''; fi') | crontab -
+(crontab -l 2>/dev/null; echo '*/5 * * * * if [ $(free -wm | awk '\''/^Mem:/ { print $8 }'\'') -le 500 ]; then XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u normal '\''Available memory: 500 Mb only!'\''; fi') | crontab - # run every 5 minutes, see `man crontab.5`
 # firefox is now main app taking more and more memory as it runs
 # after gecko install process name of firefox is GeckoMain 
 (crontab -l 2>/dev/null; echo '* * * * * if [ $(free -wm | awk '\''/^Mem:/ { print $8 }'\'') -le 250 ]; then XDG_RUNTIME_DIR=/run/user/$(id -u) pkill '\''firefox$'\'' || pkill '\''firefox-bin$'\'' || pkill GeckoMain; fi') | crontab -
