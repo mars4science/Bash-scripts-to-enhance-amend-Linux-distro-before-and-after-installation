ed8be3635948095a17bfe8979eadde347fa91c4c
Alex Martian <alex4science4mars@gmail.com>
2024-06-13 09:35:22 +0000

    added making added binaries executable (in canse were not set as such where they are copied from)

--- binaries_install.sh
+++ binaries_install.sh
@@ -4,10 +4,13 @@
 
 if [ "x${software_path_root}" = "x" ] ; then software_path_root=/media/$(id -un)/usb/LM ; fi
 
-sudo cp --no-clobber "$software_path_root"/bin/the_rest/* "$(get_install_path.sh)" # expected to have exec bit set in "$software_path_root", not setting here
+# sudo cp --no-clobber "$software_path_root"/bin/the_rest/* "$(get_install_path.sh)" # expected to have exec bit set in
+ "$software_path_root", not setting here
+sudo rsync -a --chmod=Fa+x "$software_path_root"/bin/the_rest/* "$(get_install_path.sh)"
 
 # copy apps in form of AppImage to disk and menu (to list of All Applications), follow symbolic links
-sudo cp --no-clobber --dereference "$software_path_root"/bin/appimages/* "$(get_install_path.sh)"
+# sudo cp --no-clobber --dereference "$software_path_root"/bin/appimages/* "$(get_install_path.sh)"
+sudo rsync -a -L --chmod=Fa+x "$software_path_root"/bin/appimages/* "$(get_install_path.sh)"
 sudo cp --no-clobber --dereference "$software_path_root"/bin/desktops/* /usr/share/applications
 sudo cp --no-clobber --dereference "$software_path_root"/bin/icons/* /usr/share/pixmaps
 
