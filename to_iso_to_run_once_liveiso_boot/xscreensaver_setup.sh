108abf8d4d99add1c806d490013ba1d38b2c4475
Alex Martian <alex4science4mars@gmail.com>
2024-06-27 04:19:02 +0000

    editing xscreensaver settings to point to folder with images for slideshow, which is close to zero CPU intensive as opposed to e.g. binaryring mode

--- to_iso_to_run_once_liveiso_boot/xscreensaver_setup.sh
+++ to_iso_to_run_once_liveiso_boot/xscreensaver_setup.sh
@@ -10,4 +10,5 @@ if [ -e /home/$(id -u -n) ]; then
     home_folder="/home/$(id -un)"
     cp --no-preserve=all "$software_path_root"/.xscreensaver "$home_folder"
     chmod o=r,ug=rw "$home_folder"/.xscreensaver
+    sed --in-place --regexp-extended -- "s|^imageDirectory:	.*|imageDirectory:	${software_path_root}|" "$home_folder"/.xscreensaver # favourite backgrounds are assumed to be there (e.g. used for slideshow mode, which is close to zero CPU intensive as opposed to e.g. binaryring mode)
 fi
